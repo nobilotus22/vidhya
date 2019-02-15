be9b2df60958dc70b2eda8079f7d514207232c48
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-02-15 19:28:25 +0530

    Create pset2.1.c

---
+++ pset2.1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[100];
+    scanf("%s",a);
+    if(a[0]=='s' || a[0]=='S')
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+
+    return 0;
+}
