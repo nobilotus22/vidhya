0e2b466fc1d14aec749074f68960e3c7de11edc7
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-02-20 15:52:19 +0530

    Update pset2.4.c

--- pset2.4.c
+++ pset2.4.c
@@ -4,9 +4,8 @@ int main()
 {
     int i,n;
     char a[100];
+    scanf("%d",&n);
     scanf("%s",a);
-    for(i=0;a[i]!='\0';i++);
-    n=i;
     for(i=n-1;i>=0;i--)
     {
         if(a[i]!='A' && a[i]!='E'&& a[i]!='I'&& a[i]!='O'&& a[i]!='U'&& a[i]!='a' && a[i]!='e'&& a[i]!='o'&& a[i]!='u'&& a[i]!='i')
