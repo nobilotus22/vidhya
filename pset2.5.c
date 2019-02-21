18bb943dfc049163750edd957d0ebffc9541d6dc
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-02-21 19:09:56 +0530

    Update pset2.5.c

--- pset2.5.c
+++ pset2.5.c
@@ -2,33 +2,27 @@
 
 int main()
 {
-    char a[100];
-    int i,n,j,k=0,count=0,b[100],max=0;
+    char a[100],c;
+    int i,n,j,count=0,max=0;
     scanf("%s",a);
-    for(i=0;a[i]!='\0';i++);
-    n=i;
-    for(i=0;i<n;i++)
+    for(i=0;a[i]!='\0';i++)
     {
         count==0;
-        for(j=0;j<n;j++)
+        for(j=i+1;a[j]!='\0';j++)
         {
             if(a[i]==a[j])
             {
                 count++;
+                if(max<count)
+                {
+                   max=count;
+                   c=a[i];
+                }
             }
         }
-        b[k]=count;
-        k++;
+        
     }
-    for(i=0;i<k;i++)
-    {
-        if(max<b[i])
-        {
-            max=b[i];
-            j=i;
-        }
-    }
-    printf("%c",a[j]);
+    printf("%c",c);
 
     return 0;
 }
