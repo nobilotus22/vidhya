c38c11bded58765862c38accb7397bddc4e0fe36
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-02-21 15:53:58 +0530

    Create pset2.7.c

---
+++ pset2.7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int r,l,i,min;
+    scanf("%d%d",&l,&r);
+    for(i=1;i<=l;i++)
+    {
+        if((l%i==0) && (r%i)==0)
+        {
+            min=i;
+        }
+    }
+    min=(l*r)/min;
+    printf("%d",min);
+
+    return 0;
+}
