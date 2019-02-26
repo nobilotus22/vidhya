0339ef60210b845486ca302143f53210b62ecb41
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-02-26 22:36:06 +0530

    Create pset3.6.c

---
+++ pset3.6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[1000];
+    int i,j;
+    gets(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==' ' && a[i+1]==' ')
+        {
+            for(j=i;a[j]!='\0';j++)
+            {
+                a[j+1]=a[j+2];
+            }
+        }
+    }
+    puts(a);
+    return 0;
+}
