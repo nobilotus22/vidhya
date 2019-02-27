e659ad36bb26c3c89936541794a55e4bac0cb34e
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-02-27 16:08:59 +0530

    Create pset4.2.c

---
+++ pset4.2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n,a[100],k,count=0,i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	scanf("%d",&k);
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==k)
+		{
+			printf("yes");
+			count=1;
+			break;
+		}
+	}
+	if(count==0)
+	{
+		printf("no");
+	}
+	
+	return 0;
+}
