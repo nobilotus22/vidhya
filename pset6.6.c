aae5ece62f1e8092f4b2c8f8e9205d4e27437907
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-03-10 12:15:39 +0530

    Create pset6.6.c

---
+++ pset6.6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[100],b;
+    int i,c=0;
+    scanf("%c",&b);
+    scanf("%s",a);
+    
+    for(i=0;a[i]!='\0';i++)
+    {
+       if(a[i]==b)
+       {
+           printf("%d",i+1);
+           c=1;
+           break;
+       }
+    }
+    if(c==0)
+    {
+        printf("no");
+    }
+    
+    return 0;
+}
