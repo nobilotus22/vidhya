0dcce89e828a2bb8485c21ff423b7de48b4cb181
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-03-21 22:31:05 +0530

    Create pset9.6.c

---
+++ pset9.6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+  int n,a[100],ans=0,i,j;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+      scanf("%d",&a[i]);
+  }
+ //ans=a[0];
+  for(i=0;i<n;i++)
+  {
+      ans=ans^a[i];
+  }
+  printf("%d",ans);
+  return 0;
+}
