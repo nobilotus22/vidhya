8e483dea9ef04a0941e73f635c97b39cb44073c4
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-03-21 19:41:14 +0530

    Update pset9.1.c

--- pset9.1.c
+++ pset9.1.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 
-int main(void) 
+int main()
 {
-	int n;
-	scanf("%d",&n);
-	int a[100],i,j,dif=0,max,temp;
-	for(i=0;i<n;i++)
-	{
-		scanf("%d ",&a[i]);
-	}
-	for(i=0;i<n;i++)
-	{
-		for(j=i+1;j<n;j++)
-		{
-		max=i&j;
-		if(dif<max)
-			{
-				dif=max;
-			}
-		}
-		
-	}
-	
-	printf("%d",max);
-	return 0;
+    int a[100],n,i,ans,j,max;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i;j<n;j++)
+        {
+            ans=a[i]& a[j];
+            if(max<ans)
+            {
+                max=ans;
+            }
+        }
+    }
+    printf("   %d",max);
+
+    return 0;
 }
