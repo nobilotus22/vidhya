794046546ebe44745079a298f79f020e8f8e98f3
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-03-25 07:31:22 +0530

    Create pset16.4.c

---
+++ pset16.4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main() {
+   char a[100];
+   int k,i;
+   scanf("%s",a);
+   scanf("%d",&k);
+   if(k>1)
+   {
+   for(i=1;a[i]!='\0';i++)
+   {
+       if(i%(k-1)==0)
+       {
+           a[i]=a[i]-32;
+       }
+   }}
+   else if(k==1){
+   for(i=0;a[i]!='\0';i++)
+   {
+       if(i%(k)==0)
+       {
+           a[i]=a[i]-32;
+       }
+   }
+   }
+   printf("%s",a);
+   }
