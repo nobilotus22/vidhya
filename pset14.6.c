3431bed9485aef4bc1637b3f7b51f0a0bc7a81e3
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-03-25 06:55:32 +0530

    Rename pset14.5.c to pset14.6.c

---
+++ pset14.6.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],k,i,m=0;
+    scanf("%d",&n);
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+    if(a[i]==k)
+    {
+        m++;
+    }
+}
+if(m!=0)
+{
+    printf("yes ");
+    printf("%d",m);
+}}
+    
